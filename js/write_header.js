56d947ce3d7ef03d53e03c936e87490d514ac5c7
Eileen <contact.oriontls12@gmail.com>
2025-06-21 18:48:16 -0600

    added About Page

--- js/write_header.js
+++ js/write_header.js
@@ -2,9 +2,9 @@ document.querySelector(".navHeader").innerHTML=`
 		<header>
 			<div><img src="img/title.png"></div>
 			<div id="nav">
-            	<a href="index.html" id="read">READ</a> 
-            	<a href="about.html" id="about">ABOUT</a> 
-            	<a href="characters.html" id="characters">CHARACTERS</a>
+            	<a href="index.html" id="read"><h7>READ</h7></a> 
+            	<a href="about.html" id="about"><h7>ABOUT</h7></a> 
+            	<a href="characters.html" id="characters"><h7>CHARACTERS</h7></a>
         	</div>
 		</header>
 	`;
